75daf81f5582e65f60c686d163a17a70b7b8cdcd
alex-lin64 <alex_lin@brown.edu>
2022-10-07 15:40:19 -0400

    working on spider

--- link_finder.py
+++ link_finder.py
@@ -3,21 +3,21 @@ from urllib import parse
 
 class LinkFinder(HTMLParser):
 
-    def __init__(self, homepage_url:str, page_url:str):
+    def __init__(self, homepage_url:str, page_url:str) -> None:
         super().__init__()
         self.homepage_url = homepage_url
         self.page_url = page_url
         self.links = set()
 
-    def handle_starttag(self, tag:str, attrs:str):
+    def handle_starttag(self, tag:str, attrs:str) -> None:
         if tag == 'a':
             for (attr, value) in attrs:
                 if (attr == "href"):
                     url = parse.urljoin(self.homepage_url, value)
                     self.links.add(url)
 
-    def get_page_links(self):
+    def get_page_links(self) -> set:
         return self.links
 
-    def error(self, message):
+    def error(self, message) -> None:
         pass
--- spider.py
+++ spider.py
@@ -1,2 +1,29 @@
+from urllib.request import urlopen
+from link_finder import LinkFinder
+from common import all
+
 class Spider:
-    
+
+    dir_path = ''
+    homepage_url = ''
+    domain_name = ''
+    queue_file = ''
+    crawled_file = ''
+    queue = set()
+    crawled = set()
+
+    def __init__(self, dir_path:str, homepage_url:str, domain_name:str) -> None:
+        Spider.dir_path = dir_path
+        Spider.homepage_url = homepage_url
+        Spider.domain_name = domain_name
+        Spider.queue_file = Spider.dir_path + '/queue.txt'
+        Spider.crawled_file = Spider.dir_path + '/crawled.txt'
+        self.boot()
+        self.crawl_page("init_spider", Spider.homepage_url)
+
+    def boot(self):
+        pass
+
+    def crawl_page(self):
+        pass
+
