32ef86bf395f76e295ef2f8413291dc395b17e68
alex-lin64 <alex_lin@brown.edu>
2022-10-07 13:48:26 -0400

    finished link finder

---
+++ link_finder.py
@@ -0,0 +1,23 @@
+from html.parser import HTMLParser
+from urllib import parse
+
+class LinkFinder(HTMLParser):
+
+    def __init__(self, homepage_url:str, page_url:str):
+        super().__init__()
+        self.homepage_url = homepage_url
+        self.page_url = page_url
+        self.links = set()
+
+    def handle_starttag(self, tag:str, attrs:str):
+        if tag == 'a':
+            for (attr, value) in attrs:
+                if (attr == "href"):
+                    url = parse.urljoin(self.homepage_url, value)
+                    self.links.add(url)
+
+    def get_page_links(self):
+        return self.links
+
+    def error(self, message):
+        pass
