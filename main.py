4880d8cb5c99de3b6b44f369df54160ff8d4fdb3
alex-lin64 <alex_lin@brown.edu>
2022-10-21 19:09:18 -0400

    dsahfds

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from domain import *
 from common import *
 
 
-PROJECT_DIR = "thenewboston"
-HOMEPAGE = "https://thenewboston.com/"
+PROJECT_DIR = "canvas"
+HOMEPAGE = "https://canvas.brown.edu/"
 DOMAIN_NAME = get_domain_name(HOMEPAGE)
 QUEUE_FILE = PROJECT_DIR + "/queue.txt"
 CRAWLED_FILE = PROJECT_DIR + "/crawled.txt"
 NUMBER_OF_THREADS = 2
-
 thrd_queue = Queue()
+
 Spider(PROJECT_DIR, HOMEPAGE, DOMAIN_NAME)
+
 
--- test.py
+++ test.py
@@ -1,24 +1,29 @@
+from urllib.request import Request
 from urllib.request import urlopen
 from link_finder import LinkFinder
 
 
-def gather_links(homepage_url:str, page_url:str) -> set():
-    html_str = ""
-    try:
-        response = urlopen(page_url)
-        if (response.getheader("Content-Type") == "text/html"):
-            html_bytes = response.read()
-            html_str = html_bytes.decode("utf-8")
-        finder = LinkFinder(homepage_url, page_url)
-        finder.feed(html_str)
-    except:
-        print("Error: " + page_url + " can't be crawled")
-        return set()
-    return finder.get_page_links()
+
+homepage_url = "https://www.indeed.com/"
+page_url = ""
+links = set()
+header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+req = Request(homepage_url, headers=header)
 
 
-test_set = gather_links("https://math.stackexchange.com/", "https://math.stackexchange.com/questions/4052482/residual-vectors-and-range?noredirect=1&lq=1")
-for item in test_set:
-    print(item)
-finder = LinkFinder("https://math.stackexchange.com/", "https://math.stackexchange.com/questions/4052482/residual-vectors-and-range?noredirect=1&lq=1")
-print(finder.feed("<a href=https://www.python.org/ class=nav-logo><img src=../_static/py.svg alt=Logo></a>"))
+try:
+    response = urlopen(req)
+    if (response.getheader("Content-Type") == "text/html"):
+        html_bytes = response.read()
+        html_str = html_bytes.decode("utf-8")
+    finder = LinkFinder(homepage_url, page_url)
+    finder.feed(html_str)
+    links = finder.get_page_links
+except Exception as e:
+    print(e)
+    print("Error: " + page_url + " can't be crawled")
